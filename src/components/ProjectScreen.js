64a36870f56e8a01aed1786e67149fcda5157c0e
Sam Fitz <sam@samfitz.me>
2021-08-10 20:37:44 -0400

    Refactoring

--- src/components/ProjectScreen.js
+++ src/components/ProjectScreen.js
@@ -1,29 +1,16 @@
 import React from "react";
 import {
-	Box,
-	Button,
-	Center,
 	Flex,
-	Heading,
-	HStack,
-	Input,
-	Spinner,
-	Stack,
 	Tab,
 	Tabs,
 	TabList,
 	TabPanel,
 	TabPanels,
-	Text,
-	VStack,
 } from "@chakra-ui/react";
-import { FiTrash2 } from "react-icons/fi";
 
 import EditPermissionsModal from "./EditPermissionsModal";
 import StatsScreen from "./StatsScreen";
-import TaskContent from "./TaskContent";
-import TaskDetailModal from "./TaskDetailModal";
-import useTasks from "../graphql/useTasks";
+import { TaskList } from "./TaskList";
 
 export default function ProjectScreen({
 	currentProject,
@@ -55,128 +42,4 @@ export default function ProjectScreen({
 	);
 }
 
-function useDraftTask({ addTask }) {
-	const [draftTask, setDraftTask] = React.useState(null);
-	const createDraftTask = () => {
-		setDraftTask({ name: "", description: "" });
-	};
-	const deleteDraftTask = () => {
-		setDraftTask(null);
-	};
-	const setDraftTaskName = (name) => {
-		setDraftTask({ name });
-	};
-	const setDraftTaskDescription = (description) => {
-		setDraftTask({ description });
-	};
-	const submitDraftTask = async () => {
-		await addTask(draftTask);
-		setDraftTask(null);
-	};
-	return {
-		draftTask,
-		createDraftTask,
-		deleteDraftTask,
-		setDraftTaskName,
-		submitDraftTask,
-		setDraftTaskDescription,
-	};
-}
 
-function TaskList({ currentProject }) {
-	const { tasks, addTask, loading } = useTasks(currentProject);
-	const getTaskById = (id) => tasks.find((task) => task._id === id);
-	const [selectedTaskId, setSelectedTaskId] = React.useState(null);
-	const selectedTask = getTaskById(selectedTaskId);
-
-	const {
-		draftTask,
-		createDraftTask,
-		deleteDraftTask,
-		setDraftTaskName,
-		submitDraftTask,
-		setDraftTaskDescription,
-	} = useDraftTask({ addTask });
-
-	return loading ? (
-		<Box margin="xlarge" align="center" justify="center">
-			<Spinner
-				thickness="4px"
-				speed="0.65s"
-				emptyColor="gray.200"
-				color="blue.500"
-				size="xl"
-			/>
-		</Box>
-	) : (
-		<Flex direction="column">
-			<Stack spacing={4}>
-				{tasks.length === 0 ? (
-					<Box>
-						<Heading>No Tasks</Heading>
-						<Text>Click the button below to add a task to this project</Text>
-					</Box>
-				) : (
-					tasks.map((task) => (
-						<Box onClick={() => setSelectedTaskId(task._id)}>
-							<TaskContent task={task} />
-						</Box>
-					))
-				)}
-				{draftTask ? (
-					<VStack spacing={4}>
-						<Input
-							type="text"
-							placeholder="Task Name"
-							onChange={(e) => {
-								setDraftTaskName(e.target.value);
-							}}
-							value={draftTask.name}
-						/>
-						<Input
-							type="text"
-							placeholder="Task Description"
-							onChange={(e) => {
-								setDraftTaskDescription(e.target.value);
-							}}
-							value={draftTask.description}
-						/>
-						<Box justify="center" direction="row-responsive">
-							<HStack spacing={4}>
-								<Button
-									disabled={!draftTask.name}
-									colorScheme="blue"
-									onClick={() => {
-										submitDraftTask();
-									}}
-								>
-									Add
-								</Button>
-								<Button
-									rightIcon={<FiTrash2 />}
-									colorScheme="red"
-									onClick={() => {
-										deleteDraftTask();
-									}}
-								>
-									Cancel
-								</Button>
-							</HStack>
-						</Box>
-					</VStack>
-				) : (
-					<Center>
-						<Button colorScheme="green" onClick={() => createDraftTask()}>
-							Add Task
-						</Button>
-					</Center>
-				)}
-			</Stack>
-			<TaskDetailModal
-				project={currentProject}
-				task={selectedTask}
-				unselectTask={setSelectedTaskId}
-			/>
-		</Flex>
-	);
-}
---
+++ src/components/TaskList.js
@@ -0,0 +1,109 @@
+import React from "react";
+import {
+	Box,
+	Button,
+	Center,
+	Flex,
+	Heading,
+	HStack,
+	Input,
+	Spinner,
+	Stack,
+	Text,
+	VStack,
+} from "@chakra-ui/react";
+import { FiTrash2 } from "react-icons/fi";
+import TaskContent from "./TaskContent";
+import TaskDetailModal from "./TaskDetailModal";
+import useTasks from "../graphql/useTasks";
+import { useDraftTask } from "./useDraftTask";
+
+export function TaskList({ currentProject }) {
+	const { tasks, addTask, loading } = useTasks(currentProject);
+	const getTaskById = (id) => tasks.find((task) => task._id === id);
+	const [selectedTaskId, setSelectedTaskId] = React.useState(null);
+	const selectedTask = getTaskById(selectedTaskId);
+
+	const {
+		draftTask,
+		createDraftTask,
+		deleteDraftTask,
+		setDraftTaskName,
+		submitDraftTask,
+		setDraftTaskDescription,
+	} = useDraftTask({ addTask });
+
+	return loading ? (
+		<Box margin="xlarge" align="center" justify="center">
+			<Spinner
+				thickness="4px"
+				speed="0.65s"
+				emptyColor="gray.200"
+				color="blue.500"
+				size="xl"
+			/>
+		</Box>
+	) : (
+		<Flex direction="column">
+			<Stack spacing={4}>
+				{tasks.length === 0 ? (
+					<Box>
+						<Heading>No Tasks</Heading>
+						<Text>Click the button below to add a task to this project</Text>
+					</Box>
+				) : (
+					tasks.map((task) => (
+						<Box onClick={() => setSelectedTaskId(task._id)}>
+							<TaskContent task={task} />
+						</Box>
+					))
+				)}
+				{draftTask ? (
+					<VStack spacing={4}>
+						<Input
+							type="text"
+							placeholder="Task Name"
+							onChange={(e) => setDraftTaskName(e.target.value)}
+							value={draftTask.name}
+						/>
+						<Input
+							type="text"
+							placeholder="Task Description"
+							onChange={(e) => setDraftTaskDescription(e.target.value)}
+							value={draftTask.description}
+						/>
+						<Box justify="center" direction="row-responsive">
+							<HStack spacing={4}>
+								<Button
+									disabled={!draftTask.name}
+									colorScheme="blue"
+									onClick={() => submitDraftTask()}
+								>
+									Add
+								</Button>
+								<Button
+									rightIcon={<FiTrash2 />}
+									colorScheme="red"
+									onClick={() => deleteDraftTask()}
+								>
+									Cancel
+								</Button>
+							</HStack>
+						</Box>
+					</VStack>
+				) : (
+					<Center>
+						<Button colorScheme="green" onClick={() => createDraftTask()}>
+							Add Task
+						</Button>
+					</Center>
+				)}
+			</Stack>
+			<TaskDetailModal
+				project={currentProject}
+				task={selectedTask}
+				unselectTask={setSelectedTaskId}
+			/>
+		</Flex>
+	);
+}
