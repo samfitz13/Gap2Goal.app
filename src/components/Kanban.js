75f8900d11612711cdf2326be299f3e690f3fdb5
Sam Fitz <sam@samfitz.me>
2021-08-18 17:45:19 -0400

    refactoring, formatting

--- src/components/Kanban.js
+++ src/components/Kanban.js
@@ -1,23 +1,21 @@
 import React from "react";
 import {
-	Box,
-	Button,
-	Center,
-	Flex,
-	Heading,
-	HStack,
-	Input,
-	Modal,
-	ModalBody,
-	ModalCloseButton,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
-	ModalOverlay,
-	Spinner,
-	Stack,
-	Text,
-	VStack,
+  Box,
+  Button,
+  Center,
+  Flex,
+  Heading,
+  HStack,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+  Stack,
+  VStack,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { FiTrash2 } from "react-icons/fi";
 
@@ -27,145 +25,115 @@ import useTasks from "../graphql/useTasks";
 import { useDraftTask } from "./useDraftTask";
 
 export default function Kanban({ currentProject }) {
-	const { tasks, addTask, loading } = useTasks(currentProject);
-	const getTaskById = (id) => tasks.find((task) => task._id === id);
-	const [selectedTaskId, setSelectedTaskId] = React.useState(null);
-	const selectedTask = getTaskById(selectedTaskId);
+  const { tasks, addTask, loading } = useTasks(currentProject);
+  const getTaskById = (id) => tasks.find((task) => task._id === id);
+  const [selectedTaskId, setSelectedTaskId] = React.useState(null);
+  const selectedTask = getTaskById(selectedTaskId);
 
-	const openTasks = tasks.filter((task) => task.status === "Open");
-	const inProgressTasks = tasks.filter((task) => task.status === "InProgress");
-	const completeTasks = tasks.filter((task) => task.status === "Complete");
+  let stages = ["Open", "InProgress", "Complete"];
 
-	const {
-		draftTask,
-		createDraftTask,
-		deleteDraftTask,
-		setDraftTaskName,
-		submitDraftTask,
-		setDraftTaskDescription,
-	} = useDraftTask({ addTask });
+  const {
+    draftTask,
+    createDraftTask,
+    deleteDraftTask,
+    setDraftTaskName,
+    submitDraftTask,
+    setDraftTaskDescription,
+  } = useDraftTask({ addTask });
 
-	return loading ? (
-		<Box margin="xlarge" align="center" justify="center">
-			<Spinner
-				thickness="4px"
-				speed="0.65s"
-				emptyColor="gray.200"
-				color="blue.500"
-				size="xl"
-			/>
-		</Box>
-	) : (
-		<Flex direction="column">
-			<Stack direction="Row" spacing="2">
-				<Stack spacing={4}>
-					<Heading align="center" size="md">
-						Open Tasks
-					</Heading>
-					{openTasks.length === 0 ? (
-						<Box p="3" direction="column">
-							<Heading align="center">No Open Tasks</Heading>
-							<Text align="center">
-								Click the button below to add a task to this project
-							</Text>
-						</Box>
-					) : (
-						openTasks.map((task) => (
-							<Box onClick={() => setSelectedTaskId(task._id)}>
-								<TaskContent task={task} />
-							</Box>
-						))
-					)}
-				</Stack>
-				<Stack>
-					<Heading align="center" size="md">
-						In Progress Tasks
-					</Heading>
-					{inProgressTasks.length === 0 ? (
-						<Box p="3" direction="column">
-							<Heading align="center">No In Progress Tasks</Heading>
-						</Box>
-					) : (
-						inProgressTasks.map((task) => (
-							<Box onClick={() => setSelectedTaskId(task._id)}>
-								<TaskContent task={task} statusBadgeDisplay="none" />
-							</Box>
-						))
-					)}
-				</Stack>
-				<Stack>
-					<Heading align="center" size="md">
-						Completed Tasks
-					</Heading>
-					{completeTasks.length === 0 ? (
-						<Box p="3" direction="column">
-							<Heading align="center">No In Progress Tasks</Heading>
-						</Box>
-					) : (
-						completeTasks.map((task) => (
-							<Box onClick={() => setSelectedTaskId(task._id)}>
-								<TaskContent task={task} />
-							</Box>
-						))
-					)}
-				</Stack>
-				<TaskDetailModal
-					project={currentProject}
-					task={selectedTask}
-					unselectTask={setSelectedTaskId}
-				/>
-			</Stack>
-			{draftTask ? (
-				<Modal isOpen={draftTask}>
-					<ModalOverlay />
-					<ModalContent>
-						<ModalHeader>Add Task</ModalHeader>
-						<ModalCloseButton />
-						<ModalBody>
-							<VStack spacing={4}>
-								<Input
-									type="text"
-									placeholder="Task Name"
-									onChange={(e) => setDraftTaskName(e.target.value)}
-									value={draftTask.name}
-								/>
-								<Input
-									type="text"
-									placeholder="Task Description"
-									onChange={(e) => setDraftTaskDescription(e.target.value)}
-									value={draftTask.description}
-								/>
-								<Box justify="center" direction="row-responsive">
-									<HStack spacing={4}>
-										<Button
-											disabled={!draftTask.name}
-											colorScheme="blue"
-											onClick={() => {
-												submitDraftTask();
-											}}
-										>
-											Add
-										</Button>
-										<Button
-											rightIcon={<FiTrash2 />}
-											colorScheme="red"
-											onClick={() => deleteDraftTask()}
-										>
-											Cancel
-										</Button>
-									</HStack>
-								</Box>
-							</VStack>
-						</ModalBody>
-					</ModalContent>
-				</Modal>
-			) : (
-				<Center>
-					<Button colorScheme="green" onClick={() => createDraftTask()}>
-						Add Task
-					</Button>
-				</Center>
-			)}
-		</Flex>
-	);
+  let boxColor = useColorModeValue("gray.200", "gray.900");
+
+  return loading ? (
+    <Box margin="xlarge" align="center" justify="center">
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    </Box>
+  ) : (
+    <Box>
+      <Flex direction="column" overflowX="scroll">
+        <Stack spacing={8} direction="Row">
+          {stages.map((stage) => (
+            <Box p={2} m={2} bg={boxColor}>
+              <Heading align="center" size="md">
+                {stage === "InProgress" ? "In Progress" : stage} tasks
+              </Heading>
+              {tasks.filter((task) => task.status === stage).length === 0 ? (
+                <Box align="center">
+                  No {stage === "InProgress" ? "In Progress" : stage} Tasks
+                </Box>
+              ) : (
+                tasks
+                  .filter((task) => task.status === stage)
+                  .map((task) => (
+                    <Box py={1} onClick={() => setSelectedTaskId(task._id)}>
+                      <TaskContent task={task} />
+                    </Box>
+                  ))
+              )}
+            </Box>
+          ))}
+        </Stack>
+      </Flex>
+      {draftTask ? (
+        <Modal isOpen={draftTask}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Add Task</ModalHeader>
+            <ModalBody>
+              <VStack spacing={4}>
+                <Input
+                  type="text"
+                  placeholder="Task Name"
+                  onChange={(e) => setDraftTaskName(e.target.value)}
+                  value={draftTask.name}
+                />
+                <Input
+                  type="text"
+                  placeholder="Task Description"
+                  onChange={(e) => setDraftTaskDescription(e.target.value)}
+                  value={draftTask.description}
+                />
+                <Box justify="center" direction="row-responsive">
+                  <HStack spacing={4}>
+                    <Button
+                      disabled={!draftTask.name}
+                      colorScheme="blue"
+                      onClick={() => {
+                        submitDraftTask();
+                      }}
+                    >
+                      Add
+                    </Button>
+                    <Button
+                      rightIcon={<FiTrash2 />}
+                      colorScheme="red"
+                      onClick={() => deleteDraftTask()}
+                    >
+                      Cancel
+                    </Button>
+                  </HStack>
+                </Box>
+              </VStack>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      ) : (
+        <Center mt="2">
+          <Button colorScheme="green" onClick={() => createDraftTask()}>
+            Add Task
+          </Button>
+        </Center>
+      )}
+      <TaskDetailModal
+        project={currentProject}
+        task={selectedTask}
+        unselectTask={setSelectedTaskId}
+      />
+    </Box>
+  );
 }
